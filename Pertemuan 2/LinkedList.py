b7fd7f56e84090f0c54a8e5cc70b0dbf98379656
Kafka-Alfayed <kafkanew2006@gmail.com>
2025-11-25 20:58:12 +0700

    Update LinkedList.py

--- Pertemuan 2/LinkedList.py
+++ Pertemuan 2/LinkedList.py
@@ -3,6 +3,7 @@ class Node:
         self.data = data
         self.next = pointer
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -18,13 +19,13 @@ class LinkedList:
             node_sekarang = self.head
             while node_sekarang.next:
                 node_sekarang = node_sekarang.next
-            
+
             node = Node(data)
             node_sekarang.next = node
 
     def insert_at(self, index, data):
         if index < 0 or index > self.length() - 1:
-            print("indeks tidak valid")
+            print("index tidak valid")
         elif index == 0:
             self.insert_at_first(data)
         else:
@@ -38,6 +39,42 @@ class LinkedList:
             node = Node(data, node_sekarang.next)
             node_sekarang.next = node
 
+    def remove_first(self):
+        if self.head is None:
+            print("tidak ada data yang bisa dihapus")
+        else:
+            self.head = self.head.next
+
+    def remove_last(self):
+        if self.head is None:
+            print("tidak ada data yang bisa dihapus")
+        elif self.head.next is None:
+            self.head = None
+        else:
+            node_sebelumnya = None
+            node_sekarang = self.head
+
+            while node_sekarang.next:
+                node_sebelumnya = node_sekarang
+                node_sekarang = node_sekarang.next
+
+            node_sebelumnya.next = None
+
+    def remove_at(self, index):
+        if index < 0 or index > self.length():
+            print("index invalid")
+        elif index == 0:
+            self.remove_first()
+        else:
+            urutan = 0
+            node_sekarang = self.head
+
+            while urutan < index - 1:
+                node_sekarang = node_sekarang.next
+                urutan += 1
+
+            node_sekarang.next = node_sekarang.next.next
+
     def print(self):
         if self.head is None:
             print("data kosong")
@@ -46,7 +83,7 @@ class LinkedList:
             node_sekarang = self.head
 
             while node_sekarang:
-                text_print += str(node_sekarang.data) + "→"
+                text_print += str(node_sekarang.data) + " -> "
                 node_sekarang = node_sekarang.next
 
             print(text_print)
@@ -61,16 +98,22 @@ class LinkedList:
 
         return urutan
 
-ll = LinkedList()
-#insert
-ll.insert_at_first("jeruk")
-ll.insert_at_first("mangga")
-ll.insert_at_first("manggis")
-ll.insert_at_last("apel")
-ll.insert_at(2, "durian")
-ll.insert_at_last("ikky")
-ll.insert_at(4, "zia")
 
-#print
+ll = LinkedList()
+
+# insert
+ll.insert_at_first("avanza")
+ll.insert_at_first("yaris")
+ll.insert_at_first("raize")
+ll.insert_at_last("veloz")
+ll.insert_at(2, "agya")
+
+# remove
+ll.remove_first()
+ll.remove_last()
+ll.remove_at(1)
+ll.remove_at(1)
+
+# print
 ll.print()
 print(ll.length())
